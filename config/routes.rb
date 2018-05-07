4f5840ee850f7ee8331c4e207ac2c66b880da4f0
sam13008 <ikherbane.samir@gmail.com>
2018-05-07 15:53:35 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  resources :events
   get 'pages/index'
+
+resources :devise
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#index'
+  
 end
